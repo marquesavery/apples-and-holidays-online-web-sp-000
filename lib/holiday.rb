23af8981593c11f26facc5e18bb81c2447e5560f
Marques Avery <averymarques4@gmail.com>
2020-08-11 22:16:09 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -83,9 +83,9 @@ def all_supplies_in_holidays(holiday_hash)
       end
       join_holiday = cap2_holiday.join(" ")
       puts "#{join_holiday}:"
-    else
-      cap_season = season_string.capitalize
-      puts "#{cap_season}:"
+#    else
+#      cap_season = season_string.capitalize
+#      puts "#{cap_season}:"
     end
     holiday_name.each do |holiday, supplies|
       cap_holiday = holiday.capitalize
