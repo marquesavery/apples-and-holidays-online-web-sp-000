bf3024bc9d5ffcabbac0447db0dcf4c68c573d10
Marques Avery <averymarques4@gmail.com>
2020-08-11 22:45:28 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -101,7 +101,6 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holiday_name|
     holiday_name.each do |holiday, supplies|
       if supplies.include?("BBQ")
-        binding.pry
         holiday_list << holiday_name.keys[0]
       end
     end
